700924373181d3b5f6e0717bf3d66d871878f80a
Sergey Ratiashvili <sergey.ratiashvili@aggregion.com>
2022-02-23 13:11:43 +0300

    fix: improve pausable contract test

--- src/helper/flow/must.ts
+++ src/helper/flow/must.ts
@@ -11,22 +11,29 @@ export function floateq(a: number, b: number, e: number): boolean {
 }
 
 export const mustFail = <T>(p: Promise<T>): Promise<Error> => {
-  return p
-    .then(r => {
+  return p.then(
+    (r) => {
       throw new Error(`Action should have failed but succeeded ${r}`);
-    }, () => {
-      return null
-    })
-}
+    },
+    () => {
+      return null;
+    }
+  );
+};
 
-export const mustFailWithErrorMsg = <T>(p: Promise<T>, errorMsg: String): Promise<Error> => {
-    return p
-        .then(r => {
-            throw new Error(`Action should have failed but succeeded ${r}`);
-        }, (reason) => {
-            if (!reason.message.includes(errorMsg)) {
-                throw new Error(`Action failed with invalid error ${reason}`);
-            }
-            return null
-        })
-}
+export const mustFailWithErrorMsg = <T>(
+  p: Promise<T>,
+  errorMsg: string
+): Promise<Error> => {
+  return p.then(
+    (r) => {
+      throw new Error(`Action should have failed but succeeded ${r}`);
+    },
+    (reason) => {
+      if (!reason.message.match(new RegExp(errorMsg, "gi"))) {
+        throw new Error(`Action failed with invalid error ${reason}`);
+      }
+      return null;
+    }
+  );
+};
